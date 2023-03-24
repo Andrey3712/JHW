b60560f21e1c7172683cd4f0fcf2fe44c888bfdb
Andrey3712 <adriatik61@mail.ru>
2023-03-25 00:12:49 +0300

    Решение Задачи 2 к Семинару 3

---
+++ task3_2.java
@@ -0,0 +1,31 @@
+// Задача 2: Пусть дан произвольный список целых чисел, удалить из него чётные числа
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class task3_2 {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        Random ran = new Random();
+        for (int i = 0; i < 10; i++) {
+            int x = ran.nextInt(10); // случайная переменная от 0 до 10
+            arr.add(x); // добавление переменной в массив
+        }
+        System.out.println("Начальный массив: " + arr.toString());
+
+        int i = 0;
+
+        while (i < arr.size()) {
+            if (arr.get(i) % 2 == 0) { // элемент полученный по индексу
+                arr.remove(i);
+            } else {
+                i++;
+            }
+        }
+        System.out.println("Массив без четных чисел: " + arr.toString());
+
+    }
+}
+        
